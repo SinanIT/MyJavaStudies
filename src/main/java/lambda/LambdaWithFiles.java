3dcca2940db278aadfe3fa4ef2c54d8462567dd0
SinanIT <sinandegerli@gmail.com>
2022-08-05 07:59:40 -0400

    new packages classes added.

--- src/main/java/lambda/LambdaWithFiles.java
+++ src/main/java/lambda/LambdaWithFiles.java
@@ -8,32 +8,33 @@ import java.util.Arrays;
 public class LambdaWithFiles {
     public static void main(String[] args) {
         // how to read a file with functional programming
-
-//        try {
-//            Files.lines(Paths.get("E:/inteleij/src/lambda/LambdaFile.txt")).map(String::toUpperCase).forEach(t-> System.out.println(t));
-//        } catch (IOException e) {
-//            System.out.println(" Could not read the file" + e.getMessage());
-//        }
-
-        //print all distinct words
-//        try {
-//            Files.lines(Paths.get("E:/inteleij/src/lambda/LambdaFile.txt"))
-//                    .map(t->t.split(" ")).flatMap(Arrays::stream).map(t->t.replaceAll("\\W", ""))
-//                    .distinct().forEach(t-> System.out.println(t + " "));
-//        } catch (IOException e) {
-//
-//            System.out.println(" Could not read the file" + e.getMessage());
-//        }
-
         try {
-            Files.lines(Paths.get("E:/inteleij/src/lambda/LambdaFile.txt"))
+            Files.lines(Paths.get("src/main/java/lambda/LambdaFile.txt")).map(String::toUpperCase).forEach(t-> System.out.println(t));
+        } catch (IOException e) {
+            System.out.println(" Could not read the file" + e.getMessage());
+        }
+
+        System.out.println("*****************************************************************************************");
+        //print all distinct words
+        try {
+            Files.lines(Paths.get("src/main/java/lambda/LambdaFile.txt"))
+                    .map(t->t.split(" ")).flatMap(Arrays::stream).map(t->t.replaceAll("\\W", ""))
+                    .distinct().forEach(t-> System.out.println(t + " "));
+        } catch (IOException e) {
+
+            System.out.println(" Could not read the file" + e.getMessage());
+        }
+        System.out.println("*****************************************************************************************");
+        //print all words contains character e
+        try {
+            Files.lines(Paths.get("src/main/java/lambda/LambdaFile.txt"))
                     .map(t->t.split(" ")).flatMap(Arrays::stream)
                     .map(t->t.replaceAll("\\W", ""))
                     .filter(t->t.contains("e"))
                     .forEach(System.out::println);
         } catch (IOException e) {
 
-            System.out.println(" Could not read the file" + e.getMessage());
+            System.out.println(" Could not read the file " + e.getMessage());
         }
     }
 }
